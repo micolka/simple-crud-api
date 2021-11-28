291ccc6c5c62f4fa049e9ce4fa74225bd39c3aba
micolka <mic_la@tut.by>
2021-11-28 10:20:56 +0300

    feat: add dotenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const http = require('http')
 const { handleResponses } = require('./src/crud')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8888
 
 const head = {"Content-Type": "application/json"}
 
